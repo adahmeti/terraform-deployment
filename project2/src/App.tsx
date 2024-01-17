5d4f33d2d50420643b8b8629e4c56a127f100322
Adenis Ahmeti <105243644+adahmeti@users.noreply.github.com>
2024-01-17 20:40:05 +0100

    Add files via upload

---
+++ project2/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="logo512.png" className="App-logo" alt="logo" />
+        <p>
+            In Albania we don't say hope you'll get better. We say "hajt se bahet mire" :D
+        </p>
+        <a>Follow me on GitHub: </a>
+        <a
+          className="App-link"
+          href="https://github.com/adahmeti"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          adahmeti.github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
